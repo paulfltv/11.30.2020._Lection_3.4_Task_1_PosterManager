8d51db614ff5993e9928bba261a82f9d2eb6ccd3
paulfltv <pavelfltv@yandex.ru>
2021-03-17 13:16:42 +0300

    Commit__test_added

--- src/test/java/ru/netology/manager/MoviePosterManagerTest.java
+++ src/test/java/ru/netology/manager/MoviePosterManagerTest.java
@@ -45,6 +45,43 @@ class MoviePosterManagerTest {
 	}
 
 	@Test
+	public void shouldShowNineMovies() {
+		MoviePosterManager manager = new MoviePosterManager(10);
+		MoviePoster first = new MoviePoster(
+				1, 1, "Bloodshot", "action", 16, true, 1);
+		MoviePoster second = new MoviePoster(
+				2, 2, "Onward", "animated", 6, true, 1);
+		MoviePoster third = new MoviePoster(
+				3, 3, "Hotel 'Belgrade'", "comedy", 6, true, 1);
+		MoviePoster fourth = new MoviePoster(
+				4, 4, "The Gentlemen", "action", 18, true, 1);
+		MoviePoster fifth = new MoviePoster(
+				5, 5, "The Invisible Man", "horror", 18, true,1);
+		MoviePoster sixth = new MoviePoster(
+				6, 6, "Trolls World Tour", "animated", 6, false,1);
+		MoviePoster seventh = new MoviePoster(
+				7, 7, "Number One", "comedy", 12, false,1);
+		MoviePoster eighth = new MoviePoster(
+				8, 8, "Storm Boy", "adventure", 6, false,1);
+		MoviePoster ninth = new MoviePoster(
+				9, 9, "Tosca", "music", 16, false,1);
+
+		manager.add(first);
+		manager.add(second);
+		manager.add(third);
+		manager.add(fourth);
+		manager.add(fifth);
+		manager.add(sixth);
+		manager.add(seventh);
+		manager.add(eighth);
+		manager.add(ninth);
+
+		MoviePoster[] actual = manager.getAll();
+		MoviePoster[] expected = new MoviePoster[]{ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
 	public void shouldShowFiveLastMovies() {
 		MoviePosterManager manager = new MoviePosterManager(10);
 		MoviePoster sixth = new MoviePoster(
